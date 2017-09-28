20751c7dea271db47331906cdbd030c7137eaa89
ChrisReid88 <40202859@live.napier.ac.uk>
2017-09-28 19:37:48 +0100

    Works correctly. Needs error handling

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,66 @@
 #Storing the board selections in a list
-board = [1,2,3,4,5,6,7,8,9]
+board = [0,1,2,3,4,5,6,7,8]
 
 #Displaying the board
-print " ",board[0],"|",board[1],'|',board[2]
-print " -----------"
-print " ",board[3],"|",board[4],'|',board[5]
-print " -----------"
-print " ",board[6],"|",board[7],'|',board[8]
+def display():
+    print " ",board[0],"|",board[1],'|',board[2]
+    print " -----------"
+    print " ",board[3],"|",board[4],'|',board[5]
+    print " -----------"
+    print " ",board[6],"|",board[7],'|',board[8]
 
+#Empty lists to store players selections
+xs = []
+os = []
+
+#All possible winning combinations
+win = [[1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9]]
+
+#First player/X's turn
+def xTurn():
+    x = raw_input("Player X choose a number: ")
+    x = int(x)
+
+    if board[x] != 'x' and board[x] != 'o':
+        board[x] = 'x'
+        xs.append(x)
+        xs.sort()
+    else:
+        print "This is not a valid choice. Please choose another"
+        xTurn()
+ 
+
+def oTurn():
+    o = raw_input("Player O choose a number: ")
+    o = int(o)
+
+    if board[o] != 'x' and board[o] != 'o':
+        board[o] = 'o'
+        os.append(o)
+        os.sort()
+    else:
+        print "This is not a valid choice. Please choose another"
+        oTurn()
+
+def winner():
+    if xs in win:
+        print "X's Wins!!"
+        quit()
+    if os in win:
+        print "O's Win!!"
+        quit()
+
+display()
+
+while True:
+    xTurn()
+    winner()
+    display()
+    oTurn()
+    winner()
+    display()
