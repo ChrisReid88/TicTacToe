f04b2e00c7d9c1df3c28ec3bb596fb64b9cafe33
ChrisReid88 <40202859@live.napier.ac.uk>
2017-09-28 18:21:25 +0100

    Creating/Display the board

---
+++ TicTacToe.py
@@ -0,0 +1,10 @@
+#Storing the board selections in a list
+board = [1,2,3,4,5,6,7,8,9]
+
+#Displaying the board
+print " ",board[0],"|",board[1],'|',board[2]
+print " -----------"
+print " ",board[3],"|",board[4],'|',board[5]
+print " -----------"
+print " ",board[6],"|",board[7],'|',board[8]
+
